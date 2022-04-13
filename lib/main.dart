d0672b13610e77768bc7327af83d3f77aed67d1b
Crazelu <eberelucky64@gmail.com>
2022-04-13 12:32:07 +0100

    chore: adds DevicePreview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/ui/routes/routes_generator.dart';
 import 'package:note_app/viewModels/note_view_model.dart';
@@ -6,20 +8,23 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<NoteViewModel>(
-          create: ((_) => NoteViewModel()),
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MultiProvider(
+        providers: [
+          ChangeNotifierProvider<NoteViewModel>(
+            create: ((_) => NoteViewModel()),
+          ),
+          ChangeNotifierProvider<TodoViewModel>(
+            create: ((_) => TodoViewModel()),
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          //home: Home(),
+          theme: ThemeData(),
+          onGenerateRoute: RouteGenerator.onGenerateRoute,
         ),
-        ChangeNotifierProvider<TodoViewModel>(
-          create: ((_) => TodoViewModel()),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        //home: Home(),
-        theme: ThemeData(),
-        onGenerateRoute: RouteGenerator.onGenerateRoute,
       ),
     ),
   );
